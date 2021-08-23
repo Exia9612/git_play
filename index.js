204cec1a490d2ea52fc60396695ebcc3dfe5c5be
Qipeng cheng <565644830@qq.com>
2021-08-23 22:53:04 +0800

    commit after stash

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ function add(a1, a2) {
   return a1 + a2;
 }
 
-function mul(a1, a2) {
-  return a1/a2;
-}
-
 function f2(a1) {
   return a1++;
+
+function div(a1, a2) {
+  return a1 / a2;
+}
+
+function f1 (a1) {
+  console.log(a1);
 }
