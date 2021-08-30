cc3600604a63f9fd98ea1275f12caceabfa4c840
Qipeng cheng <565644830@qq.com>
2021-08-30 22:24:31 +0800

    complete function,modify completed function, complete function2,modify function2

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ function div(a1, a2) {
 function f1 (a1) {
   console.log(a1);
 }
+
+console.log('complete function')
+console.log('change completed function')
+console.log('complete function2')
+console.log('modify function2')
